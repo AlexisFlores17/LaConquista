014d9a0e2fb3a0515628cdb97711fe534c5a4d31
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2019-04-09 22:02:04 -0500

    Correccion clases de personajes

--- Guerrero.java
+++ Guerrero.java
@@ -1,5 +1,5 @@
 public class Guerrero extends PersonajePrincipal{
 	public Guerrero(String nombre, int experiencia, int puntosDefensa, int hP, int ataque){
-		super(nombre, experiencia, puntosDefensa, hP, ataque);
+		super(nombre);
 	}
 }
